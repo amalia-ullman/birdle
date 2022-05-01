72ed19f92c9e8a7b929200f3b979141178c20c3a
amalia-ullman <mali.ullman@gmail.com>
2022-05-01 12:30:53 -0700

    didn't work but wtv

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var words = []
 var word
 var answerWord
+var currentWord = ""
+var letterSpot
+var guesses = 1
+var tiles
 
 function readTextFile(file) {
     var rawFile = new XMLHttpRequest();
@@ -17,8 +21,8 @@ function readTextFile(file) {
     rawFile.send(null);
 }
 
-readTextFile("words.txt")
-/*console.log(words)*/
+readTextFile("words.txt");
+console.log(words)
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -29,9 +33,73 @@ function getRandomWord() {
     wordValue = getRandomIntInclusive(0, words.length-1);
     word = words[wordValue];
     //console.log(word);
+    console.log(word);
+    return word;
+}
+answerWord = getRandomWord();
+console.log(answerWord);
+
+function enterLetter(key) {
+    if (currentWord.length < 5) {
+        currentWord += key;
+        letterSpot = document.getElementById(`${guesses}-${currentWord.length}`);
+        letterSpot.innerHTML = key;
+        letterSpot.style.backgroundImage = "none";
+    }
 }
 
-getRandomWord()
-setTimeout((answerWord = getRandomWord()) => {}, 100);
+key_input = (event) => {
+    console.log(event.keyCode);
+    if (event.keyCode > 64 && event.keyCode < 91) {
+        enterLetter(event.key);
+    }
+    if (event.keyCode === 8) {
+        takeback();
+    }
+    if (event.keyCode === 13) {
+        check()
+    }
+}
 
-console.log(word);
+function takeback() {
+    if (currentWord.length > 1) {
+        letterSpot = document.getElementById(`${guesses}-${currentWord.length}`);
+        letterSpot.innerHTML = "";
+        letterSpot.style.backgroundImage = 'url("tiles.png")';
+        currentWord = currentWord.slice(0, -1);
+    }
+    else {
+        letterSpot = document.getElementById(`${guesses}-${1}`);
+        letterSpot.innerHTML = "";
+        letterSpot.style.backgroundImage = 'url("tiles.png")';
+        currentWord = "";
+    }
+}
+
+
+
+
+function check() {
+    console.log("test");
+    answerWord = "hello";
+    console.log(currentWord);
+    console.log(answerWord);
+    if (currentWord.length === 5) {
+        if (currentWord === answerWord) {
+            console.log("you win.");
+        }
+        for (let x = 1; x < 6; x++) {
+            tiles = document.getElementById(`${guesses}-${x}`);
+            currentTile = document.getElementById(`${currentWord[i-1]}`)
+            if (answerWord.includes(currentWord[i-1])) {
+                if (answerWord[i-1] === currentWord[i-1]) {
+                    tiles.classList.add("green");
+                    currentTile.style.backgroundColor = "green";
+                }
+            }
+        }
+    }
+}
+
+addEventListener("keydown", key_input);
+//setTimeout((answerWord = getRandomWord()) => {}, 100);
